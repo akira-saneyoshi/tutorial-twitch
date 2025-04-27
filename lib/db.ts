ad9195e6062d589af5874d89b09181e8ae404e7a
saneyoshi <saneyoshi@collabos.com>
2025-04-27 19:39:01 +0900

    add:ngrok static domain add.

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV != "production") globalThis.prisma = db;
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export const db = prisma;
